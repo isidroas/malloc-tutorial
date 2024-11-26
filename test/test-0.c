c3ca802c95af544a781520cb2f56f9873df33be3
Isidro Arias <isidroariass@hotmail.es>
2024-11-26 14:23:37 +0100

    simplify test-0

--- test/test-0.c
+++ test/test-0.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 
 int main() {
-  int *ptr = malloc(sizeof(int));
+  long *ptr = malloc(28);
   if (ptr == NULL) { 
-    printf("Failed to malloc a single int\n");
+    printf("Failed to malloc a single int\n"); // this will call malloc(1024) and won't be freed until the end of the program. Curioso que el contenido es el mismo string fmt que le paso. TODO: check through ltrace
     return 1;
   }
 
@@ -13,19 +13,19 @@ int main() {
 
   free(ptr);
 
-  printf("malloc'd an int, assigned to it, and free'd it\n");
+  /* printf("malloc'd an int, assigned to it, and free'd it\n"); */
 
-  int *ptr2 = malloc(sizeof(int));
-  if (ptr2 == NULL) { 
-    printf("Failed to malloc a single int\n");
-    return 1;
-  }
+  /* int *ptr2 = malloc(sizeof(int)); */
+  /* if (ptr2 == NULL) {  */
+  /*   printf("Failed to malloc a single int\n"); */
+  /*   return 1; */
+  /* } */
 
-  *ptr2 = 2;
-  *ptr2 = 200;
+  /* *ptr2 = 2; */
+  /* *ptr2 = 200; */
 
-  free(ptr2);
-  printf("malloc'd an int, assigned to it, and free'd it #2\n");
+  /* free(ptr2); */
+  /* printf("malloc'd an int, assigned to it, and free'd it #2\n"); */
 
   malloc(1); // Screw up alignment.
 
@@ -39,7 +39,7 @@ int main() {
   *ptr3 = 300;
 
   free(ptr3);
-  printf("malloc'd an int, assigned to it, and free'd it #3\n");
+  /* printf("malloc'd an int, assigned to it, and free'd it #3\n"); */
 
   return 0;
 }
